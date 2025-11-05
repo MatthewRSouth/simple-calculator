0db66575d287f677250a86024a3140eae704d3bc
MatthewRSouth <matthewrogersouth@gmail.com>
2025-11-06 08:12:24 +0900

    Added function to dynamically make  equations

--- app.js
+++ app.js
@@ -8,41 +8,49 @@ const division = document.querySelector('.division');
 const screen = document.querySelector('.screen');
 
 //numbers
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const seven = document.querySelector('.seven');
-const eight = document.querySelector('.eight');
-const nine = document.querySelector('.nine');
-const zero = document.querySelector('.zero');
-const point = document.querySelector('.point');
+const one = Number(document.querySelector('.one').value);
+const two = Number(document.querySelector('.two').value);
+const three = Number(document.querySelector('.three').value);
+const four = Number(document.querySelector('.four').value);
+const five = Number(document.querySelector('.five').value);
+const six = Number(document.querySelector('.six').value);
+const seven = Number(document.querySelector('.seven').value);
+const eight = Number(document.querySelector('.eight').value);
+const nine = Number(document.querySelector('.nine').value);
+const zero = Number(document.querySelector('.zero').value);
+const point = document.querySelector('.point').value;
 const clear = document.querySelector('.clear');
 const equals = document.querySelector('.equals');
 
 //button pressed function
-function buttonPressed(buttonContent) {
-    screen.textContent = buttonContent;
-    if (buttonContent === 'clear') {
-        screen.textContent = '';
-    }
+//x = first number that is pressed
+
+function equation(x, operand, y) {
+    //This function builds the equation dynamically
+    //1. Get first input and set it = x
+    //2. get operand input and set it equal to operand
+    //3. get second input and set it = y w
 }
+
+function buttonPressed(x, operand, y) {
+    // if
+}
+console.log(three);
+//Need to listen for any button pressed
 //button selectors
-one.addEventListener('click', () => buttonPressed('1'));
-two.addEventListener('click', () => buttonPressed('2'));
-three.addEventListener('click', () => buttonPressed('3'));
-four.addEventListener('click', () => buttonPressed('4'));
-five.addEventListener('click', () => buttonPressed('5'));
-six.addEventListener('click', () => buttonPressed('6'));
-seven.addEventListener('click', () => buttonPressed('7'));
-eight.addEventListener('click', () => buttonPressed('8'));
-nine.addEventListener('click', () => buttonPressed('9'));
-zero.addEventListener('click', () => buttonPressed('0'));
-addition.addEventListener('click', () => buttonPressed('+'));
-subtraction.addEventListener('click', () => buttonPressed('-'));
-multiplication.addEventListener('click', () => buttonPressed('x'));
-division.addEventListener('click', () => buttonPressed('÷'));
-clear.addEventListener('click', () => buttonPressed('clear'));
-point.addEventListener('click', () => buttonPressed('.'));
+// one.addEventListener('click', () => buttonPressed(1));
+// two.addEventListener('click', () => buttonPressed(2));
+// three.addEventListener('click', () => buttonPressed(3));
+// four.addEventListener('click', () => buttonPressed(4));
+// five.addEventListener('click', () => buttonPressed(5));
+// six.addEventListener('click', () => buttonPressed(6));
+// seven.addEventListener('click', () => buttonPressed(7));
+// eight.addEventListener('click', () => buttonPressed(8));
+// nine.addEventListener('click', () => buttonPressed(9));
+// zero.addEventListener('click', () => buttonPressed(0));
+// addition.addEventListener('click', () => buttonPressed('+'));
+// subtraction.addEventListener('click', () => buttonPressed('-'));
+// multiplication.addEventListener('click', () => buttonPressed('x'));
+// division.addEventListener('click', () => buttonPressed('÷'));
+// clear.addEventListener('click', () => buttonPressed('clear'));
+// point.addEventListener('click', () => buttonPressed('.'));
